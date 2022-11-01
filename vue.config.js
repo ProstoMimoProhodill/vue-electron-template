589c7de08c71bb6c4cf9b99398bc9638cbe37caa
ProstoMimoProhodill <keshe4k@yandex.ru>
2022-11-01 16:42:41 +0300

    prime vue resolver

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service');
+const { PrimeVueResolver } = require('unplugin-vue-components/resolvers');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      require('unplugin-vue-components/resolvers')({
+        resolvers: [PrimeVueResolver],
+      }),
+    ],
+  },
 });
