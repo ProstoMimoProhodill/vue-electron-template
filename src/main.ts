3b6bceba269c3df00f292a727ef3952567cadd27
ProstoMimoProhodill <keshe4k@yandex.ru>
2022-11-01 17:31:16 +0300

    styles

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
 app.use(PrimeVue);
+
+// eslint-disable-next-line vue/multi-word-component-names
+app.component('Button', Button);
+
 app.mount('#app');
